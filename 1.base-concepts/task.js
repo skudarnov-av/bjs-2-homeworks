02fe07fc17ef84a71e1a3f0e905af4519c0f94d6
aleks skudarnov <skudarnovfred@mail.ru>
2023-05-14 20:21:11 +0500

    README task_part1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+	let arr = []
+	if (a == 0)
+		return arr
+	let dis = b ** 2 - 4 * a * c;
+	console.log('D = ' + dis);
+	if (dis < 0)
+		return arr;
+	if (dis == 0)
+		arr.push(-b / (2 * a));
+	if (dis > 0) {
+		arr.push((-b + Math.sqrt(dis)) / (2 * a));
+		arr.push((-b - Math.sqrt(dis)) / (2 * a));
+	}
+	return arr;
+};
+console.log(solveEquation());
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+	let mPercent = (percent / 100) / 12;
+	let bodyCredit = amount - contribution;
+	let mPayment = bodyCredit * (mPercent + (mPercent / (((1 + mPercent) ** countMonths) - 1)));
+	let tAmount = mPayment * countMonths;
+	let total = tAmount.toFixed(2);
+	return parseFloat(total);
+	
+	
+};
+console.log(calculateTotalMortgage());
