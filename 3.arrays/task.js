1e94793c07d055639b6ad9cb604b0b84dca06a9a
aleks skudarnov <skudarnovfred@mail.ru>
2023-05-20 12:37:02 +0500

    Домашнее задание Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,102 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 }
+console.log(compareArrays([8, 1, 2], [8, 1, 2]));
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let filter = users.filter(user => user.gender === gender);
+	if (filter.length === 0) {
+		return 0;
+	}
+	let total = filter.reduce((sum, user) => sum + user.age, 0);
+	let avg = total / filter.length;
+	return avg;
 }
+
+const people = [{
+		firstName: "Александр",
+		secondName: "Карпов",
+		age: 17,
+		gender: "мужской"
+	},
+	{
+		firstName: "Егор",
+		secondName: "Морозов",
+		age: 21,
+		gender: "мужской"
+	},
+	{
+		firstName: "Мелисса",
+		secondName: "Леонова",
+		age: 40,
+		gender: "женский"
+	},
+	{
+		firstName: "Мелания",
+		secondName: "Савельева",
+		age: 37,
+		gender: "женский"
+	},
+	{
+		firstName: "Мария",
+		secondName: "Овчинникова",
+		age: 18,
+		gender: "женский"
+	},
+	{
+		firstName: "Марьяна",
+		secondName: "Котова",
+		age: 17,
+		gender: "женский"
+	},
+	{
+		firstName: "Фёдор",
+		secondName: "Селезнев",
+		age: 50,
+		gender: "мужской"
+	},
+	{
+		firstName: "Георгий",
+		secondName: "Петров",
+		age: 35,
+		gender: "мужской"
+	},
+	{
+		firstName: "Даниил",
+		secondName: "Андреев",
+		age: 49,
+		gender: "мужской"
+	},
+	{
+		firstName: "Дарья",
+		secondName: "Савельева",
+		age: 25,
+		gender: "женский"
+	},
+	{
+		firstName: "Михаил",
+		secondName: "Шаров",
+		age: 22,
+		gender: "мужской"
+	},
+	{
+		firstName: "Владислав",
+		secondName: "Давыдов",
+		age: 40,
+		gender: "мужской"
+	},
+	{
+		firstName: "Илья",
+		secondName: "Казаков",
+		age: 35,
+		gender: "мужской"
+	},
+	{
+		firstName: "Евгений",
+		secondName: "Кузьмин",
+		age: 19,
+		gender: "мужской"
+	},
+];
+
+console.log(getUsersNamesInAgeRange(people, "мужской"));
